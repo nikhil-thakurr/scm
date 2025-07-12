c5562329fc25be751a65557239c846a6dbff4df7
Nikhil Thakur <nt34542@gmail.com>
2025-07-12 17:01:10 +0530

    Entities Built and some views also

---
+++ src/main/java/com/scm/entities/User.java
@@ -0,0 +1,46 @@
+package com.scm.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "user")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    private String userId;
+    @Column(name = "user_name", nullable = false)
+
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Getter(AccessLevel.NONE)
+    private String password;
+    @Column(length = 1000)
+    private String about;
+    @Column(length = 1000)
+    private String profilePic;
+    private String phoneNumber;
+    private boolean emailVerified = false;
+    private boolean phoneVerified = false;
+
+     @Enumerated(value = EnumType.STRING)
+    // SELF, GOOGLE, FACEBOOK, TWITTER, LINKEDIN, GITHUB
+    private Providers provider = Providers.SELF;
+    private String providerUserId;
+
+}
